29d9d3fbf94f1d3ce4ce2cf4bb9ff9ab5230220c
marcio lira <machollanda@gmail.com>
2020-10-10 14:59:32 -0300

    exercises

--- 01_Day_JavaScript_Refresher/introduction.js
+++ 01_Day_JavaScript_Refresher/introduction.js
@@ -351,6 +351,286 @@
 // }
 // This fruit does not exist in the arra
 
+//___________________________________________________________________________
+// Getting last index of an element in array
 
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 3, 1, 2]
+
+// console.log(numbers.lastIndexOf(2)) // 7
+// console.log(numbers.lastIndexOf(0)) // -1
+// console.log(numbers.lastIndexOf(1)) //  6
+// console.log(numbers.lastIndexOf(4)) //  3
+// console.log(numbers.lastIndexOf(6)) // -1
+
+// includes:To check if an item exist in an array. If it exist it returns the true else it returns false.
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// console.log(numbers.includes(5)) // true
+// console.log(numbers.includes(0)) // false
+// console.log(numbers.includes(1)) // true
+// console.log(numbers.includes(6)) // false
+
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB',
+// ] // List of web technologies
+
+// console.log(webTechs.includes('Node')) // true
+// console.log(webTechs.includes('C')) // false
+
+// Checking array
+
+// Array.isArray:To check if the data type is an array
+
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(Array.isArray(numbers)) // true
+
+// const number = 100
+// console.log(Array.isArray(number)) // false
+
+// Joining array elements
+
+// join: It is used to join the elements of the array, the argument we passed in the join method will be joined in the array and return as a string. By default, it joins with a comma, but we can pass different string parameter which can be joined between the items.
+
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(numbers.join()) // 1,2,3,4,5
+
+// const names = ['claudio', 'Marcio', 'lira', 'de holanda']
+
+// console.log(names.join()) // Asabeneh,Mathias,Elias,Brook
+// console.log(names.join('')) //AsabenehMathiasEliasBrook
+// console.log(names.join(' ')) //Asabeneh Mathias Elias Brook
+// console.log(names.join(', ')) //Asabeneh, Mathias, Elias, Brook
+// console.log(names.join(' # ')) //Asabeneh # Mathias # Elias # Brook
+
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB',
+// ] // List of web technologies
+
+// console.log(webTechs.join()) // "HTML,CSS,JavaScript,React,Redux,Node,MongoDB"
+// console.log(webTechs.join(' # ')) // "HTML # CSS # JavaScript # React # Redux # Node # MongoDB"
+
+// Slice array elements
+
+// Slice: To cut out a multiple items in range. It takes two parameters:starting and ending position. It doesn't include the ending position.
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// console.log(numbers.slice()) // -> it copies all  item
+// console.log(numbers.slice(0)) // -> it copies all  item
+// console.log(numbers.slice(0, numbers.length)) // it copies all  item
+// console.log(numbers.slice(1, 4)) // -> [2,3,4] // it doesn't include the ending position
+
+// Splice method in array
+
+// Splice: It takes three parameters:Starting position, number of times to be removed and number of items to be added.
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// console.log(numbers.splice()) // -> remove all items
+
+// const numbers1 = [1, 2, 3, 4, 5]
+// console.log(numbers1.splice(0, 1)) // remove the first item
+
+// const numbers2 = [1, 2, 3, 4, 5, 6]
+// console.log(numbers2.splice(3, 3, 7, 8, 9)) // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
+
+//________________________________
+
+//  --- Adding item to an array using push --- 
+
+// Push: adding item in the end. To add item to the end of an existing array we use the push method.
+
+// syntax
+// const arr = ['item1', 'item2', 'item3']
+// arr.push('new item')
+
+// console.log(arr)
+// // ['item1', 'item2','item3','new item']
+
+// const numbers = [1, 2, 3, 4, 5]
+// numbers.push(6)
+
+// console.log(numbers) // -> [1,2,3,4,5,6]
+
+// numbers.pop() // -> remove one item from the end
+// console.log(numbers) // -> [1,2,3,4,5]
+
+// let fruits = ['banana', 'orange', 'mango', 'lemon']
+// fruits.push('apple')
+
+// console.log(fruits) // ['banana', 'orange', 'mango', 'lemon', 'apple']
+
+// fruits.push('lime')
+
+
+// console.log(fruits) // ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime']
+
+// --- Removing the end element using pop --- 
+
+// pop: Removing item in the end.
+
+// const numbers = [1, 2, 3, 4, 5]
+// numbers.pop() // -> remove one item from the end
+
+// console.log(numbers) // -> [1,2,3,4]
+
+// --- Removing an element from the beginning ----
+
+// shift: Removing one array element in the beginning of the array.
+
+// const numbers = [1, 2, 3, 4, 5]
+// numbers.shift() // -> remove one item from the beginning
+
+// console.log(numbers) // -> [2,3,4,5]
+
+// ---- Add an element from the beginning ----
+
+// unshift: Adding array element in the beginning of the array.
+
+// const numbers = [1, 2, 3, 4, 5]
+// numbers.unshift(0) // -> add one item from the beginning
+
+// console.log(numbers) // -> [0,1,2,3,4,5]
+
+// ---- Reversing array order ----
+
+// reverse: reverse the order of an array.
+
+// const numbers = [1, 2, 3, 4, 5]
+// numbers.reverse() // -> reverse array order
+
+// console.log(numbers) // [5, 4, 3, 2, 1]
+
+// numbers.reverse()
+// console.log(numbers) // [1, 2, 3, 4, 5]
+
+// ---- Sorting elements in array ----
+
+// sort: arrange array elements in ascending order. Sort takes a call back function, we will see how we use sort with a call back function in the coming sections.
+
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB',
+// ]
+
+// webTechs.sort()
+// console.log(webTechs) // ["CSS", "HTML", "JavaScript", "MongoDB", "Node", "React", "Redux"]
+
+// webTechs.reverse() // after sorting we can reverse it
+// console.log(webTechs) // ["Redux", "React", "Node", "MongoDB", "JavaScript", "HTML", "CSS"]
+
+// webTechs.shift() // Remove o primeiro item
+// console.log(webTechs)
+
+// ---- Array of arrays ----
+
+// Array can store different data types including an array itself. Let us create an array of arrays
+
+// const firstNums = [1, 2, 3]
+// const secondNums = [1, 4, 9]
+
+// const arrayOfArray = [
+//   [1, 2, 3],
+//   [1, 2, 3],
+// ]
+// console.log(arrayOfArray[0]) // [1, 2, 3]
+
+// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+// const backEnd = ['Node', 'Express', 'MongoDB']
+// const fullStack = [frontEnd, backEnd]
+
+// console.log(fullStack) // [["HTML", "CSS", "JS", "React", "Redux"], ["Node", "Express", "MongoDB"]]
+// console.log(fullStack.length) // 2
+// console.log(fullStack[0]) // ["HTML", "CSS", "JS", "React", "Redux"]
+// console.log(fullStack[1]) // ["Node", "Express", "MongoDB"]
+
+// ---- 💻 Exercise ----
+// Exercise: Level 1
+
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+]
+
+
+// Declare an empty array;
+const arr = []
+console.log(arr)
+
+
+// Declare an array with more than 5 number of elements
+const a = ['j','a','v','a','s','c','r','i','p','t']
+console.log(a)
+
+
+// Find the length of your array
+
+console.log(a.length)
+// Get the first item, the middle item and the last item of the array
+
+const first = a[0]
+const middle = a[4]
+const last = a[9]
+
+console.log(last)
+console.log(middle)
+console.log(first)
+// Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+
+// Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+// Print the array using console.log()
+// Print the number of companies in the array
+// Print the first company, middle and last company
+// Print out each company
+// Change each company name to uppercase one by one and print them out
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+// Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+// Filter out companies which have more than one 'o' without the filter method
+// Sort the array using sort() method
+// Reverse the array using reverse() method
+// Slice out the first 3 companies from the array
+// Slice out the last 3 companies from the array
+// Slice out the middle IT company or companies from the array
+// Remove the first IT company from the array
+// Remove the middle IT company or companies from the array
+// Remove the last IT company from the array
+// Remove all IT companies
 
 
