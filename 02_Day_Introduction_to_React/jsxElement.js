ddcfbb5337e247d2911a9c4f7490a5363d9bac8b
marcio lira <machollanda@gmail.com>
2020-10-21 23:12:14 -0300

    Day2

---
+++ 02_Day_Introduction_to_React/jsxElement.js
@@ -0,0 +1,36 @@
+const jsxElement = <h1>I am a JSX element</h1>
+const welcome = <h1>Welcome to 30 Days of React Challenge</h1>
+const data = <small>Oct 2, 2020</small>
+
+//JSX Element
+
+const jsxElement = <h1>I am a JSX element</h1> // JS with HTML
+const title = <h2>Getting Started React</h2>
+
+const header = (
+    <header>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+    </header>
+  )
+
+ // Commenting a JSX element
+
+//We comment codes for different reasons and it is also good to know how to comment out JSX elements in React.
+
+{
+  /*
+ <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Asabeneh Yetayeh</p>
+    <small>Oct 2, 2020</small>
+  </header>
+
+*/
+}
+
+//Rendering a JSX Element
+
