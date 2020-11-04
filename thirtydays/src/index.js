da7bdec949cf8fb570d5c707d7325bb2f4471a32
marcio lira <machollanda@gmail.com>
2020-11-03 23:46:42 -0300

    importing media objects

--- thirtydays/src/index.js
+++ thirtydays/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import picImagem from '../Users/holan/Pictures/pic.jpeg'
 
 
 ReactDOM.render(
@@ -97,89 +97,97 @@ ReactDOM.render(
 
 // INJECTING DATA TO JSX ELEMENTS
 
-const welcome = 'Welcome to 30 Days Of React'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
-const author = {
-  firstName: 'Claudio',
-  lastName: 'Holanda',
-}
+// const welcome = 'Welcome to 30 Days Of React'
+// const title = 'Getting Started React'
+// const subtitle = 'JavaScript Library'
+// const author = {
+//   firstName: 'Claudio',
+//   lastName: 'Holanda',
+// }
 
-const date = 'Oct 2, 2020'
+// const date = 'Oct 2, 2020'
 
-// JSX element, header
-const header = (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div>
-  </header>
-)
+// // JSX element, header
+// const header = (
+//   <header>
+//     <div className='header-wrapper'>
+//       <h1>{welcome}</h1>
+//       <h2>{title}</h2>
+//       <h3>{subtitle}</h3>
+//       <p>
+//         Instructor: {author.firstName} {author.lastName}
+//       </p>
+//       <small>Date: {date}</small>
+//     </div>
+//   </header>
+// )
 
-const numOne = 3
-const numTwo = 2
+// const numOne = 3
+// const numTwo = 2
 
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
-)
+// const result = (
+//   <p>
+//     {numOne} + {numTwo} = {numOne + numTwo}
+//   </p>
+// )
 
-const yearBorn = 1820
-const currentYear = new Date().getFullYear()
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-)
+// const yearBorn = 1820
+// const currentYear = new Date().getFullYear()
+// const age = currentYear - yearBorn
+// const personAge = (
+//   <p>
+//     {' '}
+//     {author.firstName} {author.lastName} is {age} years old
+//   </p>
+// )
 
-// JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+// // JSX element, main
+// const techs = ['HTML', 'CSS', 'JavaScript']
+// const techsFormatted = techs.map((tech) => <li>{tech}</li>)
 
-// JSX element, main
-const main = (
-  <main>
-    <div className='main-wrapper'>
-      <p>
-        Prerequisite to get started{' '}
-        <strong>
-          <em>react.js</em>
-        </strong>
-        :
-      </p>
-      <ul>{techsFormatted}</ul>
-      {result}
-      {personAge}
-    </div>
-  </main>
-)
+// // JSX element, main
+// const main = (
+//   <main>
+//     <div className='main-wrapper'>
+//       <p>
+//         Prerequisite to get started{' '}
+//         <strong>
+//           <em>react.js</em>
+//         </strong>
+//         :
+//       </p>
+//       <ul>{techsFormatted}</ul>
+//       {result}
+//       {personAge}
+//     </div>
+//   </main>
+// )
 
-const copyRight = 'Copyright 2020'
+// const copyRight = 'Copyright 2020'
 
-// JSX element, footer
-const footer = (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>{copyRight}</p>
-    </div>
-  </footer>
-)
+// // JSX element, footer
+// const footer = (
+//   <footer>
+//     <div className='footer-wrapper'>
+//       <p>{copyRight}</p>
+//     </div>
+//   </footer>
+// )
 
-// JSX element, app
-const app = (
-  <div className='app'>
-    {header}
-    {main}
-    {footer}
+// // JSX element, app
+// const app = (
+//   <div className='app'>
+//     {header}
+//     {main}
+//     {footer}
+//   </div>
+// )
+
+// Importing Media Objects in React
+
+const user = (
+  <div>
+    <img src={picImagem} alt='asabeneh image' />
   </div>
 )
 
@@ -195,6 +203,7 @@ const rootElement = document.getElementById('root')
 ReactDOM.render(jsxElement, rootElement)
 ReactDOM.render(header, rootElement)
 ReactDOM.render(app, rootElement)
+ReactDOM.render(user, rootElement)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
