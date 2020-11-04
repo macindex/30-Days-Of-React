9d85e07f381f9291c7a514ba08abf2deda0b318d
marcio lira <machollanda@gmail.com>
2020-11-03 23:24:52 -0300

    add jsx elements

--- thirtydays/src/index.js
+++ thirtydays/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,12 +13,74 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const jsxElement = <h1>This is a JSX element</h1>
+const headerStyles = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
+
+const header = (
+  <header style={headerStyles}>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Claudio Holanda</p>
+      <small>Oct 2, 2020</small>
+    </div>
+  </header>
+)
+
+
+
+// JSX element, main
+const mainStyles = {
+  backgroundColor: '#F3F0F5',
+}
+const main = (
+  <main style={mainStyles}>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+// JSX element, footer
+const footerStyles = {
+  backgroundColor: '#61DBFB',
+}
+// JSX element, footer
+const footer = (
+  <footer style={footerStyles}>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+
+const jsxElement = <h1>This is a JSX element claudio</h1>
+// const title = <h2>Getting Started React</h2>
 const rootElement = document.getElementById('root')
 
 
-ReactDOM.render(jsxElement, rootElement)
 
+
+
+ReactDOM.render(jsxElement, rootElement)
+ReactDOM.render(header, rootElement)
+ReactDOM.render(app, rootElement)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
